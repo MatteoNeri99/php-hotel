585fd1734f761ce3c635a87b1add34e0865781ca
MatteoNeri99 <matteolavoro99@gmail.com>
2024-06-27 17:05:58 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+
+
+?>
+
+
+<div> <?php foreach($hotels as $hotel) {?>
+
+
+    <p> <?php echo $hotel["name"] ?> </p>
+    <p> <?php echo $hotel["description"] ?> </p>
+    <p> <?php echo $hotel["parking"] ?> </p>
+    <p> <?php echo $hotel["vote"] ?> </p>
+    <p> <?php echo $hotel["distance_to_center"] ?> </p>
+
+
+
+
+
+    <?php } ?>
+</div>
