3c1c04c3a45fc89d928ddb91d4a62a9f291de4ea
MatteoNeri99 <matteolavoro99@gmail.com>
2024-06-28 08:09:56 +0200

    aggiunto form con checkbox e ciclo for

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
     $hotels = [
 
         [
@@ -42,23 +40,109 @@
 
     ];
 
+    $visible="";
+
+    $parcheggio=$_GET["parcheggio"];
+
+    $hotelBuono=$_GET["stelle"];
+
+    $controllo=false;
+
+    var_dump($parcheggio);
+
+
+    for($i=0; $i<count($hotels); $i++){
+
+        $hotel=$hotels[$i];
+
+        $parking=$hotel["parking"];
+
+        $voto=$hotel["vote"];
+
+        var_dump($parking);
+
+        if($parcheggio == "1" && $parking == false){
+            $visible="invisible";
+
+        }elseif($parcheggio !== "1" && $parking !== false){
+            $visible="visible";
+
+        };
+
+        if($hotelBuono == "1" && $voto >= 3){
+            $visible="invisible";
+
+        }elseif($hotelBuono !== "1" && $voto <= 3){
+            $visible="visible";
+
+        };
+    
+
+
+
+    }
+
 
 
 ?>
 
 
-<div> <?php foreach($hotels as $hotel) {?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+ 
+    <title>Hotel-php</title>
+</head>
+<body>
+
+    <main>
 
 
-    <p> <?php echo $hotel["name"] ?> </p>
-    <p> <?php echo $hotel["description"] ?> </p>
-    <p> <?php echo $hotel["parking"] ?> </p>
-    <p> <?php echo $hotel["vote"] ?> </p>
-    <p> <?php echo $hotel["distance_to_center"] ?> </p>
+    <form action="./index.php" method="GET">
 
+        <label for="parcheggio">ha il parcheggio</label>
+        <input type="checkbox" name="parcheggio" id="parcheggio" value="1">
 
+        <label for="stelle">con 3 stelle o più</label>
+        <input type="checkbox" name="stelle" id="stelle" value="1">
 
+        <button type="submit">invia</button>
 
+    </form>
 
-    <?php } ?>
-</div>
+        <div class="container"> 
+            <table class="table"><?php foreach($hotels as $hotel) {?>
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col <?php echo $visible?>"> <?php echo $hotel["name"] ?> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row" >1</th>
+                    <td class="<?php echo $visible?>"> <?php echo $hotel["description"] ?> </td>
+                    </tr>
+                    <tr >
+                    <th scope="row">2</th>
+                    <td class="<?php echo $visible?>"> <?php echo $hotel["vote"] ?> </td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td class="<?php echo $visible?>"> <?php echo $hotel["distance_to_center"] ?> </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+       
+        </div>
+    </main>
+        
+</body>
+</html>
+
